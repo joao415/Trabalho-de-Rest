5c0f8a0dc5e7a58136eee69fd93622297412ecef
João da Silva e Souza <joao8816@outlook.com>
2023-06-22 21:12:45 -0300

    (Kelvin 22/06/2023) feat: Terminar
    (Kelvin 22/06/2023) feat: Terminar

--- src/main/java/com/repositorio/AlunoRepositorio.java
+++ src/main/java/com/repositorio/AlunoRepositorio.java
@@ -1,13 +1,14 @@
 package com.repositorio;
-
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.data.modelo.AlunoModelo;
-import com.data.modelo.UsuarioModelo;
-
-public interface AlunoRepositorio extends CrudRepository<AlunoModelo, Integer> {
-    List<UsuarioModelo> findAllByDisciplina(String nome);
-    List<UsuarioModelo> findAllByProfessor(String nascimento);
-}
+//package com.repositorio;
+//
+//import java.util.List;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.data.modelo.AlunoModelo;
+//import com.data.modelo.UsuarioModelo;
+//
+//public interface AlunoRepositorio extends CrudRepository<AlunoModelo, Integer> {
+//    List<UsuarioModelo> findAllByDisciplina(String nome);
+//    List<UsuarioModelo> findAllByProfessor(String nascimento);
+//}
